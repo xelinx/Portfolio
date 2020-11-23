b9bc06930bea7b846f0ae5f910ff1f5672d37863
Fog <lin.emay@gmail.com>
2020-11-23 04:33:41 -0800

    update routing, add component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Nav from "./components/Nav";
+import Project from "./components/Project";
+import Main from "./components/Main";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Container fluid>
+          <Switch>
+            <Route path="/project" exact>
+              <Project />
+            </Route>
+            <Route path="/" exact>
+              <Main />
+            </Route>
+            <Route path="/contact" exact>
+              <Contact />
+            </Route>
+          </Switch>
+        </Container>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
